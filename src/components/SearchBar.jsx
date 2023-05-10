60be0ed35c79d008c1db3ca02f0a4f94193a8689
diego34346 <diego_f423@hotmail.com>
2023-05-09 21:15:59 -0500

    lerning

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,11 @@
+import style from "./SearchBar.module.css"
+
 export default function SearchBar(props) {
    // const key = () => {}
    return (
       <div>
          <input type='search' />
-         <button id="add" onClick={props.onSearch}><span>Agregar</span></button>
+         <button className={style.add} onClick={props.onSearch}><span className={style.span}>Agregar</span></button>
       </div>
    );
 }
