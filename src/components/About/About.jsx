6500ccab5bea181be96ec16667b723b2472d8855
diego34346 <diego_f423@hotmail.com>
2023-06-03 20:12:54 -0500

    About-icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import style from './about.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function About(props){
     return(
@@ -7,8 +10,26 @@ export default function About(props){
             <div className={style.containAbout} >
                 <img src="avatar.png" alt="about" />  
                 <div>
-                <h1>Hello!!</h1>
-                <p>I'm Diego, a full stack web developer, I have knowledge in Javascript, Html, CSS, Node, React, Redux, SQL, Git, and more...</p>
+                    <h1>Hello!!</h1>
+                    <p>I'm Diego, a full stack web developer, I have knowledge in Javascript, Html, CSS, Node, React, Redux, SQL, Git, and more...</p>
+
+                    <div className={style.containIcon} >
+                        <Link
+                            className={style.brandIcon}
+                            to="https://www.linkedin.com/in/diegofeliperosas/"
+                            target="_blank"
+                            >
+                            <FontAwesomeIcon className={style.icon} icon={faLinkedin} />
+                        </Link>
+
+                        <Link
+                            className={style.brandIcon}
+                            to="https://github.com/diego34346"
+                            target="_blank"                    
+                            >
+                            <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                            </Link>
+                    </div>
                 </div>
             </div>        
         </div>
