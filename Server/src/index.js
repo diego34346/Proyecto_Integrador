92555d5ba5b53d73af6a72e6ba22d84bfda9fd37
diego34346 <diego_f423@hotmail.com>
2023-06-13 23:14:24 -0500

    Integration_myServer-Api

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,29 +1,46 @@
-const http = require("http");
-const data = require("./utils/data")
-var fs = require("fs");
-
-
-module.exports = 
+const http = require ("http")
+const getCharById = require ("./controllers/getCharById")
 
 http
   .createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    console.log(req.url)
-
     if(req.url.includes("/rickandmorty/character")){        
         const id = req.url.split("/").at(-1)
-        const character = data.find((char) => char.id == id)
+        getCharById(res,id)
+    }    
+  }).listen(3001, ("localhost"))
 
-        if(character){
-            res.writeHead(200, {"Content-Type" : "application/json"})
-            return res.end(JSON.stringify(character))
-        }else{
-            res.writeHead(404,{"Content-Type": "application/json"})
-            return res.end(JSON.stringify({error:"Character not found"}))
-        }
-    }
-    res.writeHead(404,{"Content-Type":"text-plain"})
-    res.end("Route not found")
+
+
+
+
+// const http = require("http");
+// const data = require("./utils/data")
+// var fs = require("fs");
+
+
+// module.exports = 
+
+// http
+//   .createServer((req, res) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+
+//     console.log(req.url)
+
+//     if(req.url.includes("/rickandmorty/character")){        
+//         const id = req.url.split("/").at(-1)
+//         const character = data.find((char) => char.id == id)
+
+//         if(character){
+//             res.writeHead(200, {"Content-Type" : "application/json"})
+//             return res.end(JSON.stringify(character))
+//         }else{
+//             res.writeHead(404,{"Content-Type": "application/json"})
+//             return res.end(JSON.stringify({error:"Character not found"}))
+//         }
+//     }
+//     res.writeHead(404,{"Content-Type":"text-plain"})
+//     res.end("Route not found")
     
-  }).listen(3001, "localhost");
+//   }).listen(3001, "localhost");
