99d5dc4bb48d4282b0d604d176239e4bf0668ef5
diego34346 <diego_f423@hotmail.com>
2023-06-30 22:10:28 -0500

    process.env-Server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const server = express();
-const PORT = 3001;
 const router = require('./routes/index')
 const morgan = require('morgan')
+require('dotenv').config()
+const { PORT_SERVER } = process.env
 
 
 server.use((req, res, next) => {
@@ -27,55 +28,6 @@ server.get("/",(req, res) => {
 
 server.use("/rickandmorty", router)
 
-server.listen(PORT, () => {
-  console.log(`server raised in port ${PORT}`);
+server.listen(PORT_SERVER, () => {
+  console.log(`server raised in port ${PORT_SERVER}`);
 })
-
-
-
-
-
-
-// const http = require ("http")
-// const getCharById = require ("./controllers/getCharById")
-
-// http
-//   .createServer((req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-
-//     if(req.url.includes("/rickandmorty/character")){
-//         const id = req.url.split("/").at(-1)
-//         getCharById(res,id)
-//     }
-//   }).listen(3001, ("localhost"))
-
-
-
-// const http = require("http");
-// const data = require("./utils/data")
-// var fs = require("fs");
-
-// module.exports =
-
-// http
-//   .createServer((req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-
-//     console.log(req.url)
-
-//     if(req.url.includes("/rickandmorty/character")){
-//         const id = req.url.split("/").at(-1)
-//         const character = data.find((char) => char.id == id)
-
-//         if(character){
-//             res.writeHead(200, {"Content-Type" : "application/json"})
-//             return res.end(JSON.stringify(character))
-//         }else{
-//             res.writeHead(404,{"Content-Type": "application/json"})
-//             return res.end(JSON.stringify({error:"Character not found"}))
-//         }
-//     }
-//     res.writeHead(404,{"Content-Type":"text-plain"})
-//     res.end("Route not found")
-
-//   }).listen(3001, "localhost");
